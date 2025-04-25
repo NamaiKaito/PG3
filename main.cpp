6203cddde60774f6e40b6d56accd84aea3e8ea5b
Kaito_Namai <k024g1004@g.neec.ac.jp>
2025-04-25 16:53:19 +0900

    コミット

--- main.cpp
+++ main.cpp
@@ -9,17 +9,28 @@ int Recursive(int hour) {
 }
 
 int main() {
-	int hours = 10;
+	int hours = 1;
 	int standardWage = 1072;
 	int totalStandardWage = standardWage * hours;
-	int totalRecursiveWage = 0;
+	int totalRecursiveWage;
 
-	for (int i = 1; i <= hours; i++) {
-		totalRecursiveWage += Recursive(i);
+	while (1) {
+		totalStandardWage = standardWage * hours;
+		totalRecursiveWage = 0;
+
+		for (int i = 1; i <= hours; i++) {
+			totalRecursiveWage += Recursive(i);
+		}
+
+		if (totalRecursiveWage > totalStandardWage) {
+			break;
+		}
+		hours++;
 	}
 
-	printf("一般的な賃金体系での合計賃金：%d円\n", totalStandardWage);
+	printf("再帰的な賃金体系の方が儲かるのは、%d時間働いた時です。\n", hours);
 	printf("再帰的な賃金体系での合計賃金：%d円\n", totalRecursiveWage);
-
+	printf("一般的な賃金体系での合計賃金：%d円\n", totalStandardWage);
+	
 	return(0);
 }
