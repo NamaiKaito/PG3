3e8b5b5e2faa6c673fda39ab62cd41ba1fa0f95d
Kaito_Namai <k024g1004@g.neec.ac.jp>
2025-04-25 16:36:01 +0900

    コミット

--- main.cpp
+++ main.cpp
@@ -1,16 +1,45 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
 
-void PrintHelloWorld(void) {
-	printf("HelloWorld\n");
+typedef void(*PFunc)(const char*);
+
+int rollDice() {
+	return(rand() % 6) + 1;
 }
-int main(void) {
 
-	void (*pfunc)();
+void showResult(const char* result) {
+	printf("%s\n", result);
+}
 
-	pfunc = PrintHelloWorld;
-	printf("PrintHelloWorldのアドレス = %p\n", PrintHelloWorld);
-	printf("pfuncの内容 = %p\n", *pfunc);
-	pfunc();
+void checkGuess(int guess, int diceResult, PFunc callback) {
+	const char* result;
 
-		return 0;
+	if ((diceResult % 2 == 0 && guess == 0) || (diceResult % 2 != 0 && guess == 1)) {
+		result = "正解";
+	} else {
+		result = "不正解";
+	}
+	Sleep(3000);
+	callback(result);
+}
+
+int main() {
+	srand((unsigned int)time(NULL));
+
+	int diceResult = rollDice();
+	int userGuess;
+
+	printf("サイコロの目が半か丁かを予想してください。\n");
+	printf("0:丁,1:半\n");
+	printf("予想してください:");
+
+	if (scanf("%d", &userGuess) != 1 || (userGuess != 1)) {
+		printf("無効な入力です。0か1で予想してください。\n");
+		return 1;
+	}
+	checkGuess(userGuess, diceResult, showResult);
+
+	return 0;
 }
