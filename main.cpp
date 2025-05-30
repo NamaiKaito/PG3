901fa8c295c59b9db86d002f7ed5a73706430426
Kaito_Namai <k024g1004@g.neec.ac.jp>
2025-05-30 14:10:50 +0900

    コミット

--- main.cpp
+++ main.cpp
@@ -1,27 +1,2 @@
-#include <stdio.h>
-#include "IShape.h"
-#include "Circle.h"
-#include "Rectangle.h"
-
-int main(void) {
-
-	IShape* shape[2];
-
-	shape[0] = new Circle(4);
-	shape[1] = new Rectangle(2, 3);
-
-	printf("円の半径:4\n短形の底辺:2\n短形の高さ:3\n");
-
-	for (int i = 0; i < 2; i++)
-		shape[i]->Size();
-
-
-	printf("\n");
-
-	for (int i = 0; i < 2; i++)
-		shape[i]->Draw();
-
-	return 0;
-}
 
 
