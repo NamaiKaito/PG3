a632a41dfd02cf7b6b6c306c8b45f4c2b9ecc85d
Kaito_Namai <k024g1004@g.neec.ac.jp>
2025-07-25 16:11:55 +0900

    komitto

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
-#include<stdio.h>
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void PrintThread(uint32_t num) {
+	cout << "thread" << num << endl;
+}
 
 int main() {
+	thread t1(PrintThread, 1);
+	t1.join(); // © t1‚ð‘Ò‹@
 
-	printf("Hello PG3 00_01");
+	thread t2(PrintThread, 2);
+	t2.join(); // © t2‚ð‘Ò‹@
+
+	thread t3(PrintThread, 3);
+	t3.join(); // © t3‚ð‘Ò‹@
 
 	return 0;
 }
